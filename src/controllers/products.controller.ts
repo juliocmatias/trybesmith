069eb3f1fde10297207240bc4faf4e1a79063015
Julio Matias <juliocesark2@hotmail.com>
2024-03-25 14:29:30 -0300

    feat: req 01 ok

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -5,7 +5,7 @@ import mapStatusHTTP from '../utils/mapStatusHTTP';
 const create = async (req: Request, res: Response) => {
   const { name, price, userId } = req.body;
 
-  const { status, data } = await productsService.create({ name, price, userId });
+  const { status, data } = await productsService.create({ name, price, userId: Number(userId) });
 
   return res.status(mapStatusHTTP(status)).json(data);
 };
