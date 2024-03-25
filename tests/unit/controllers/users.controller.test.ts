37847e74d5ee76e9e7f9a93c51b73ae182b583bf
Julio Matias <juliocesark2@hotmail.com>
2024-03-25 17:37:24 -0300

    test: teste da userController finalizados

--- tests/unit/controllers/users.controller.test.ts
+++ tests/unit/controllers/users.controller.test.ts
@@ -2,6 +2,9 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import { UserSequelizeModel } from '../../../src/database/models/user.model';
+import { usersService } from '../../../src/services';
+import { usersController } from '../../../src/controllers';
 
 chai.use(sinonChai);
 
@@ -13,6 +16,30 @@ describe('UsersController', function () {
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
     sinon.restore();
+  });
+
+  describe('getAll', function () {
+    it('should return all users', async function () {
+      // triple A
+      // Arrange
+      const users: UserSequelizeModel[] = [
+        { id: 1, username: 'user1', vocation: 'mage', level: 1, password: 'password' } as unknown as UserSequelizeModel,
+        { id: 2, username: 'user2', vocation: 'warrior', level: 2, password: 'password' } as unknown as UserSequelizeModel,
+      ];
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+      sinon.stub(usersService, 'getAll').resolves({ status: 'SUCCESSFUL', data: users });
+
+      // Act
+
+      await usersController.getAll(req, res);
+
+      // Assert
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(users);
+    });
   });
 
 });
