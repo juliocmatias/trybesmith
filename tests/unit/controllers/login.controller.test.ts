1134ae600c690644d96b35ff03f394a129e6208a
Julio Matias <juliocesark2@hotmail.com>
2024-03-25 21:47:52 -0300

    test: implementando teste loginController

--- tests/unit/controllers/login.controller.test.ts
+++ tests/unit/controllers/login.controller.test.ts
@@ -2,6 +2,9 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import { loginService } from '../../../src/services';
+import { loginController } from '../../../src/controllers';
+import { user } from '../../mocks/login';
 
 chai.use(sinonChai);
 
@@ -15,4 +18,25 @@ describe('LoginController', function () {
     sinon.restore();
   });
 
+  it.only('should return a token if the user exists', async function () {
+    // triple A
+    // Arrange
+    
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJIYWdhciIsImlhdCI6MTY4Njc1NDc1Nn0.jqAuJkcLp0RuvrOd4xKxtj_lm3Z3-73gQQ9IVmwE5gA";
+
+    req.body = user;
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+    sinon.stub(loginService, 'login').resolves({ status: 'SUCCESSFUL', data: { token } });
+
+    // Act
+
+    await loginController.login(req, res);
+    
+    // Assert
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith({ token });
+  });
 });
