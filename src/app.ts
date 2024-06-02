109aea905f2ff8c345b868e043ff468f82a15831
Julio Matias <juliocesark2@hotmail.com>
2024-06-02 01:18:27 -0300

    feat: implement swagger ui

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import SwaggerUI from 'swagger-ui-express';
+import swaggerJSON from './swagger.json';
 import { productsRouter, usersRouter, loginRouter } from './routers';
 
 const app = express();
+
+app.use('/api-doc', SwaggerUI.serve, SwaggerUI.setup(swaggerJSON));
 
 app.use(express.json());
 app.use('/products', productsRouter);
